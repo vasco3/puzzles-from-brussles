147176f3f7c10fbb4a0faee0ecba389c99cf8d00
Jorge Cuadra <jorgecuadrafueyo@gmail.com>
2017-04-02 22:03:52 -0700

    Credits and deploy to prod

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # puzzles-from-brussles
-Puzzle game
+
+> Puzzle game
+
+## Overview
+
+- [Live Demo](https://puzzle.cuadranteweb.com)
+
+> This is a puzzle game enabled by drag and drop
+
+## Tech stack
+
+- ReactJS
+- ReactDnD for drag and drop
+- NextJS for isomorphic setup
+
+## To install locally
+
+- git clone <repo>
+- in CLI run `npm run dev`
+- or `npm run build` and `npm run start`
+- browse `localhost:3000`
+
+## Credits
+
+- Created by [Jorge Cuadra](https://www.linkedin.com/in/cuadraman/)
+- Photography by [Scott Umstattd](https://unsplash.com/@scott_umstattd)
+
--- components/Board/Board.js
+++ components/Board/Board.js
@@ -6,9 +6,14 @@ import Square from '../Square/Square'
 import { DragDropContext } from 'react-dnd';
 
 function Board(props) {
+  // Collection of pieces indexed by their position for quicker access.
   const piecesByPosition = props.pieces.reduce((positionGraph, piece) => {
+    // Only displaying in the board the pieces for the board
     if (piece.container === 'board') {
-      return { ...positionGraph, [piece.position]: piece };
+      return {
+        ...positionGraph,
+        [piece.position]: piece
+      };
     }
     return positionGraph;
   }, {});
@@ -18,6 +23,8 @@ function Board(props) {
       <div className="board">
         {
           Array.from({ length: props.piecesCount }, (value, position) => {
+            // Alternate background color in squares to
+            // create the checkers pattern.
             const x = position % 10;
             const y = Math.floor(position / 10);
             const black = (x + y) % 2 === 1;
@@ -28,7 +35,7 @@ function Board(props) {
                         dispatcher={props.dispatcher}
                         piece={piecesByPosition[position]}
                         position={position}>
-                  {piecesByPosition[position] && (
+                  { piecesByPosition[position] && (
                     <Piece dispatcher={props.dispatcher}
                            pictureUrl={props.pictureUrl}
                            piece={piecesByPosition[position]} />
@@ -43,6 +50,7 @@ function Board(props) {
       <div className="stack">
         <div className="stack-pieces">
         { props.pieces.filter(piece => piece.container === 'stack')
+          /* Show the pieces that are pending to drag to the board. */
             .map((piece, index) => (
               <Piece key={piece.container + index}
                      dispatcher={props.dispatcher}
@@ -62,7 +70,7 @@ function Board(props) {
             width: 500px;
           }
           .stack {
-            background-color: #1af;
+            background-color: rgba(17, 170, 255, 0.58);
             height: 60px;
             margin-top: 20px;
             overflow-x: scroll;
